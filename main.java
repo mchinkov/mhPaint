43f26ff5df3c496e9283af6b17463fa50ad4887f
michaelchinkov <michael.chinkov@icloud.com>
2025-12-14 21:41:03 -0800

    Added Icon

--- main.java
+++ main.java
@@ -1,15 +1,19 @@
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 public class Main {
 
     public static final int widthWindow = 420;
     public static final int lengthWindow = 420;
+    
 
     public static void main(String[] args) {
+        ImageIcon image = new ImageIcon("Paint Brush Image.png");
         JFrame frame = new JFrame();
 
         frame.setTitle("mhPaint");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setIconImage(image.getImage());
 
         frame.setSize(widthWindow,lengthWindow);
         frame.setVisible(true);
