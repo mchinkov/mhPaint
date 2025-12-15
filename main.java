1cb58c7bb5af822d6f1c5733d8291e28f517fc00
michaelchinkov <michael.chinkov@icloud.com>
2025-12-14 21:30:34 -0800

    Created the basics for the program using Swing
    Co-authored-by: haoqisx <haoqisx@users.noreply.github.com>

--- main.java
+++ main.java
@@ -1,3 +1,17 @@
-public class main {
-    
+import javax.swing.JFrame;
+
+public class Main {
+
+    public static final int widthWindow = 420;
+    public static final int lengthWindow = 420;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+
+        frame.setTitle("mhPaint");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.setSize(widthWindow,lengthWindow);
+        frame.setVisible(true);
+    }
 }
